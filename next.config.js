75d4bd63887a8956bc530d40cac6786e491e4630
HarlamovEvgeniy <harlamov.mail@mail.ru>
2022-07-08 01:39:26 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  useFileSystemPublicRoutes: true,
+  useFileSystemPublicRoutes: false,
   future: {
     webpack5: true
   }
