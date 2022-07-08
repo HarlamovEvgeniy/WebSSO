214cfff992b3b50b09fae82d6cec72f0d83cd47c
HarlamovEvgeniy <harlamov.mail@mail.ru>
2022-07-08 04:34:57 +0300

    Update auth.routes.js

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -12,32 +12,37 @@ router.get('/auth', cors(), async (req, res) => {
       req.session.message = utils.generateMessage();
       req.session.token = req.sessionID;
 
-      try {
-        fs.writeFileSync(`./session/${req.sessionID}.json`, JSON.stringify({
-          message: req.session.message,
-          isAuth: false
-        }, null, 2));
-      } catch (err) { console.log('Session Error', err); }
+      const QRCode = {
+        endpoint: req.session.endpoint || null,
+        method: req.session.method || null,
+        message: req.session.message || null,
+        sessionToken: req.session.token || null
+      }
+
+      const response = {
+        url: ('http://185.225.35.119:5000/?auth=')
+      }
+
+      // const response = {
+      //   url: ('http://185.225.35.119:5000/?auth=' + btoa(JSON.stringify(QRCode))).toString()
+      // }
+
+      res.statusCode = 200;
+      return res.json(response);
+
+      // try {
+      //   fs.writeFileSync(`./session/${req.sessionID}.json`, JSON.stringify({
+      //     message: req.session.message,
+      //     isAuth: false
+      //   }, null, 2));
+      // } catch (err) { console.log('Session Error', err); }
     } else {
       res.statusCode = 302;
       return res.json('No Query Endpoint & No Query Method');
     }
 
-    const QRCode = {
-      endpoint: req.session.endpoint || null,
-      method: req.session.method || null,
-      message: req.session.message || null,
-      sessionToken: req.session.token || null
-    }
-
-    const response = {
-      url: ('http://185.225.35.119:5000/?auth=' + btoa(JSON.stringify(QRCode))).toString()
-    }
-
-    res.statusCode = 200;
-    return res.json(response);
   } catch (error) { 
-    res.statusCode = 502;
+    res.statusCode = 500;
     return res.json(error);
   } 
 })
