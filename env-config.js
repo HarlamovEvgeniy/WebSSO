7c5f0939d7dc74cb449adb45464250bb16fa4c08
HarlamovEvgeniy <harlamov.mail@mail.ru>
2022-07-31 12:42:27 +0300

    Server Fix

--- env-config.js
+++ env-config.js
@@ -1,7 +1,7 @@
-const prod = true;
+const prod = process.env.NODE_ENV == 'production';
 
 module.exports = {
   'process.env.BACKEND_URL': prod ? '/web-sso' : '',
-  '_PORT': prod ? 443 : 3000,
+  '_PORT': prod ? parseInt(process.env.PORT, 10) : 3000,
   '_HOST': prod ? 'https://sso-defispace.ru/' : 'http://localhost'
 }
--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const next = require('next');
-const dev = process.env.NODE_ENV !== 'production';
 const session = require('express-session');
 const RedisStorage = require('connect-redis')(session);
 const Redis = require('redis');
 const clientSession = Redis.createClient({ legacyMode: true });
 const { libNode } = require('@tonclient/lib-node');
 const { initSettings } = require('everscale-did-sdk-radiance');
+const dev = process.env.NODE_ENV !== 'production';
 const nextServer = next({ dev });
 const handle = nextServer.getRequestHandler();
-const { _PORT, _HOST} = require('./env-config');
+const { _PORT, _HOST } = require('./env-config');
 const { initConnect } = require('./utils/storage/redis');
 
 nextServer.prepare().then(() => {
@@ -38,7 +38,7 @@ nextServer.prepare().then(() => {
   });
 
   app.listen(_PORT, async (req, res) => {
-    console.log('Server Started on PORT: ' + _PORT);
+    console.log('Server Started on PORT: ' + _PORT + '& HOST: ' + _HOST);
     await clientSession.connect();
     initSettings("devNet", libNode);
     await initConnect();
