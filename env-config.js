2535d8cf23c96730a5ba4dae12615147098f34e5
HarlamovEvgeniy <harlamov.mail@mail.ru>
2022-07-31 14:00:12 +0300

    Update env-config.js

--- env-config.js
+++ env-config.js
@@ -2,6 +2,6 @@ const prod = process.env.NODE_ENV == 'production';
 
 module.exports = {
   'process.env.BACKEND_URL': prod ? '/web-sso' : '',
-  '_PORT': process.env.NODE_PORT || 3000,
+  '_PORT': 3000,
   '_HOST': prod ? 'https://sso-defispace.ru/' : 'http://localhost'
 }
