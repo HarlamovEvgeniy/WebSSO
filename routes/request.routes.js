c6ddf4e5df69ddbfe5bf74826562ad899dcba41f
Hacker-rue <pavlik.soldatov.12@gmail.com>
2022-08-01 17:12:13 +0400

    fix server

--- routes/request.routes.js
+++ routes/request.routes.js
@@ -35,7 +35,10 @@ router.get('/auth', cors(), async (req, res) => {
               did: json.did
             })).toString()
             console.log("URL: " + url)
-            res.setHeader("Access-Control-Allow-Origin", "*")
+            response.setHeader("Access-Control-Allow-Origin", "*");
+            response.setHeader("Access-Control-Allow-Credentials", "true");
+            response.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+            response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
             res.redirect(url)
           } else {
             res.sendStatus(501)
